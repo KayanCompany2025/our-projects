cc06211dd1dd0dbc0b287abc805d21a538197c29
KayanCompany2025 <medohytham183@gmail.com>
2025-07-24 01:52:16 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,270 @@
+const translation = {
+  en: {
+    Home:  "Home",
+    Our_projects: "Our Projects",
+    Type_of_floor: "Type of floor That we install",
+    about_us: "About us",
+    contact_us: "Contact us",
+    title: "Projects",
+    see_photos: "See photos",
+    p_1 : "Amazon Cairo Festival City location\n\n- LVT flooring installation.\n- Installation of carpet flooring tiles.\n- Vinyl flooring installation.",
+    p_2 : "Egyptian Stock Exchange Building Smart Village location\n\n- Installation of raised floors.",
+    p_3: "Sangor University Burj AL Arab location\n\n- Installation of vinyl floors."
+    ,see_photos_2: "See photos",
+    see_photos_3: "See photos",
+    see_photos_4: "See photos",
+    see_photos_5: "See photos",
+    see_photos_6: "See photos",
+    see_photos_7: "See photos",
+    see_photos_8: "See photos",
+    see_photos_9: "See photos",
+    see_photos_10: "See photos",
+    see_photos_11: "See photos",
+    see_photos_12: "See photos",
+    see_photos_13: "See photos",
+    see_photos_14: "See photos",
+    see_photos_15: "See photos",
+    see_photos_16: "See photos",
+    see_photos_17: "See photos",
+    see_photos_18: "See photos",
+    see_photos_19: "See photos",
+    see_photos_20: "See photos",
+    p_4: "British School SODC location\n\n- Installation of vinyl floors.",
+    p_5:"Medical center nasr city location\n\n-Vinyl Floors installation.\n-LVT flooring installation",
+    p_6:"Mount international School\n\n-Vinyl flooring installation.\n-Carpet flooring installation.\n-Installing parquet flooring.\n",
+    p_7:"Medical center Wessal city location\n\n-Vinyl Floors installation.\n",
+    p_8:"Administrative building Diaa Company\n\n-Vinyl flooring installation.",
+    p_9:"People Hospital location\n\n-Vinyl Floors installation.\n",
+    p_10:"Administrative building hvd\n location fifth settlement\n\n-Vinyl Floors installation.\n",
+    p_11:"Ijada Company building\n location smart village\n\n-Vinyl Floors tiles installation.\n",
+    p_12:"Haven Hospital\n\n-Vinyl Floors installation.",
+    p_13:"japanese University\n location Borg EL Arab\n\n-installation raised floors."
+    ,p_14:"Universal School\n\n-installation of playground floors.",
+    p_15:"jayda Center\nlocation nasr city\n\n-Vinyl flooring installation.",
+    p_16:"B12 Hospital\nlocation Madinaty\n\n-Vinyl flooring installation.",
+    p_17:"king School\n\n-Vinyl flooring installation.",
+    p_18:"Egyptain Post\nlocation Smart village\n\n-Vinyl flooring installation and carpeting.",
+    p_19:"Benha University\n\n-Installation of carpet flooring tiles for the University theater.",
+    p_20:"Cairo Specialized Hospital\n\n-Installation of vinyl flooring on floors and walls."
+ 
+
+
+  },
+  ar: {
+    Home:  "الصفحة الرئيسية",
+    Our_projects: "مشاريعنا",
+    Type_of_floor: "نوع الأرضيات التي نقوم بتركيبها",
+    about_us: "معلومات عنا",
+    contact_us: "اتصل بنا",
+    title: "مشاريعنا",
+    see_photos: "عرض الصور",
+    p_1 : "أمازون موقع كايرو فستيفال سيتي\n\n- LVT تركيب أرضيات .\n- تركيب بلاط أرضيات موكيت بلاطات.\n- تركيب أرضيات فينيل.",
+    p_2: "مبنى البورصة المصرية موقع القرية الذكية\n\n- تركيب أرضيات مرتفعة.",
+    p_3: "جامعة سنجر برج العرب\n\n- تركيب أرضيات فينيل .",
+    see_photos_2: "عرض الصور",
+    see_photos_3: "عرض الصور",
+    see_photos_4: "عرض الصور",
+    see_photos_5: "عرض الصور",
+    see_photos_6: "عرض الصور",
+    see_photos_7: "عرض الصور",
+    see_photos_8: "عرض الصور",
+    see_photos_9: "عرض الصور",
+    see_photos_10: "عرض الصور",
+    see_photos_11: "عرض الصور",
+    see_photos_12: "عرض الصور",
+    see_photos_13: "عرض الصور",
+    see_photos_14: "عرض الصور",
+    see_photos_15: "عرض الصور",
+    see_photos_16: "عرض الصور",
+    see_photos_17: "عرض الصور",
+    see_photos_18: "عرض الصور",
+    see_photos_19: "عرض الصور",
+    see_photos_20: "عرض الصور",
+    p_4: "المدرسة البريطانية \n SODC موقع  \n\n- تركيب أرضيات  فينيل."
+    ,p_5:"المركز الطبي \n مدينة نصر\n\n- تركيب أرضيات  فينيل.\n- LVT تركيب أرضيات.",
+    p_6:"مدرسة ماونت الدولية\n\n- تركيب أرضيات فينيل.\n- تركيب بلاط أرضيات موكيت.\n- تركيب أرضيات الباركيه.\n",
+    p_7:"المركز الطبي  \n موقع مدينة الوصال \n\n- تركيب أرضيات  فينيل.\n",
+    p_8:"المبنى الإداري لشركة ضيا\n\n- تركيب أرضيات فينيل.",
+    p_9:"مستشفى الناس\n\n- تركيب أرضيات فينيل.\n",
+    p_10:"hvd المبنى الإداري \n موقع التجمع الخامس\n\n- تركيب أرضيات فينيل.\n",
+    p_11:"مبنى مركز إجايدا\n  موقع مدينة نصر\n\n- تركيب أرضيات فينيل \n",
+    p_12:"مستشفى هافن\n\n- تركيب أرضيات فينيل.",
+    p_13:"الجامعة اليابانية\n موقع برج العرب\n\n- تركيب أرضيات مرتفعة."
+    ,p_14:"universal مدرسة\n\n- تركيب أرضيات ملعب.",
+    p_15:"jayda مركز\nموقع القرية الذكية\n\n- تركيب أرضيات فينيل.",
+    p_16:"B12 مستشفى\nموقع مدينتي\n\n- تركيب أرضيات فينيل.",
+    p_17:"king مدرسة\n\n- تركيب أرضيات فينيل.",
+    p_18:"البريد المصري\nموقع القرية الذكية\n\n- تركيب أرضيات فينيل و موكيت.",
+    p_19:"جامعة بنها\n\n- تركيب بلاط أرضيات موكيت لمسرح الجامعة.",
+    p_20:"مستشفى القاهرة التخصصي\n\n- تركيب أرضيات فينيل على الأرضيات والجدران."
+
+
+
+  }
+}
+
+
+const languageselectop = document.querySelector("select");
+let p_1 = document.getElementById("p_1");
+let Home = document.getElementById("Home");
+let Our_projects = document.getElementById("Our_projects");
+let Type_of_floor = document.getElementById("Type_of_floor");
+let contact_us = document.getElementById("contact_us");
+let about_us = document.getElementById("about_us")
+let title = document.getElementById("title");
+let p_2 = document.getElementById("p_2");
+let p_3 = document.getElementById("p_3");
+let see_photos = document.getElementById("see_photos");
+let see_photos_2 = document.getElementById("see_photos_2");
+let see_photos_3 = document.getElementById("see_photos_3");
+let see_photos_4 = document.getElementById("see_photos_4");
+let see_photos_5 = document.getElementById("see_photos_5");
+let see_photos_6 = document.getElementById("see_photos_6");
+let see_photos_7 = document.getElementById("see_photos_7");
+let see_photos_8 = document.getElementById("see_photos_8");
+let see_photos_9 = document.getElementById("see_photos_9");
+let see_photos_10 = document.getElementById("see_photos_10");
+let see_photos_11 = document.getElementById("see_photos_11");
+let see_photos_12 = document.getElementById("see_photos_12");
+let see_photos_13 = document.getElementById("see_photos_13");
+let see_photos_14 = document.getElementById("see_photos_14");
+let see_photos_15 = document.getElementById("see_photos_15");
+let see_photos_16 = document.getElementById("see_photos_16");
+let see_photos_17 = document.getElementById("see_photos_17");
+let see_photos_18 = document.getElementById("see_photos_18");
+let see_photos_19 = document.getElementById("see_photos_19");
+let see_photos_20 = document.getElementById("see_photos_20");
+let p_4 = document.getElementById("p_4");
+let p_5 = document.getElementById("p_5");
+let p_6 = document.getElementById("p_6");
+let p_7 = document.getElementById("p_7");
+let p_8 = document.getElementById("p_8");
+let p_9 = document.getElementById("p_9");
+let p_10 = document.getElementById("p_10");
+let p_11 = document.getElementById("p_11");
+let p_12 = document.getElementById("p_12");
+let p_13 = document.getElementById("p_13");
+let p_14 = document.getElementById("p_14");
+let p_15 = document.getElementById("p_15");
+let p_16 = document.getElementById("p_16");
+let p_17 = document.getElementById("p_17");
+let p_18 = document.getElementById("p_18");
+let p_19 = document.getElementById("p_19");
+let p_20 = document.getElementById("p_20");
+
+
+
+
+
+
+
+languageselectop.addEventListener("change",(event) => {setlanguage(event.target.value)})
+const setlanguage = (language) => {
+  if(language == "ar"){
+    p_1.innerText = translation.ar.p_1;
+    Home.innerText = translation.ar.Home;
+    Our_projects.innerText = translation.ar.Our_projects;
+    Type_of_floor.innerText = translation.ar.Type_of_floor;
+    contact_us.innerText = translation.ar.contact_us;
+    title.innerText = translation.ar.title;
+    about_us.innerText = translation.ar.about_us;
+    p_2.innerText = translation.ar.p_2;
+    p_3.innerText = translation.ar.p_3;
+    see_photos.innerText = translation.ar.see_photos;
+    see_photos_2.innerText = translation.ar.see_photos_2;
+    see_photos_3.innerText = translation.ar.see_photos_3;
+    see_photos_4.innerText = translation.ar.see_photos_4;
+    see_photos_5.innerText = translation.ar.see_photos_5;
+    see_photos_6.innerText = translation.ar.see_photos_6;
+    see_photos_7.innerText = translation.ar.see_photos_7;
+    see_photos_8.innerText = translation.ar.see_photos_8;
+    see_photos_9.innerText = translation.ar.see_photos_9;
+    see_photos_10.innerText = translation.ar.see_photos_10;
+    see_photos_11.innerText = translation.ar.see_photos_11;
+    see_photos_12.innerText = translation.ar.see_photos_12;
+    see_photos_13.innerText = translation.ar.see_photos_13;
+    see_photos_14.innerText = translation.ar.see_photos_14;
+    see_photos_15.innerText = translation.ar.see_photos_15;
+    see_photos_16.innerText = translation.ar.see_photos_16;
+    see_photos_17.innerText = translation.ar.see_photos_17;
+    see_photos_18.innerText = translation.ar.see_photos_18;
+    see_photos_19.innerText = translation.ar.see_photos_19;
+    see_photos_20.innerText = translation.ar.see_photos_20;
+    p_4.innerText = translation.ar.p_4;
+    p_5.innerText = translation.ar.p_5;
+    p_6.innerText = translation.ar.p_6;
+    p_7.innerText = translation.ar.p_7;
+    p_8.innerText = translation.ar.p_8;
+    p_9.innerText = translation.ar.p_9;
+    p_10.innerText = translation.ar.p_10;
+    p_11.innerText = translation.ar.p_11;
+    p_12.innerText = translation.ar.p_12;
+    p_13.innerText = translation.ar.p_13;
+    p_14.innerText = translation.ar.p_14;
+    p_15.innerText = translation.ar.p_15;
+    p_16.innerText = translation.ar.p_16;
+    p_17.innerText = translation.ar.p_17;
+    p_18.innerText = translation.ar.p_18;
+    p_19.innerText = translation.ar.p_19;
+    p_20.innerText = translation.ar.p_20;
+
+
+
+
+
+  }else if(language == "en"){
+    p_1.innerText = translation.en.p_1;
+    Home.innerText = translation.en.Home;
+    Our_projects.innerText = translation.en.Our_projects;
+    Type_of_floor.innerText = translation.en.Type_of_floor;
+    contact_us.innerText = translation.en.contact_us;
+    title.innerText = translation.en.title;
+    about_us.innerText = translation.en.about_us;
+    p_2.innerText = translation.en.p_2;
+    p_3.innerText = translation.en.p_3;
+    see_photos.innerText = translation.en.see_photos;
+    see_photos_2.innerText = translation.en.see_photos_2;
+    see_photos_3.innerText = translation.en.see_photos_3;
+    see_photos_4.innerText = translation.en.see_photos_4;
+    see_photos_5.innerText = translation.en.see_photos_5;
+    see_photos_6.innerText = translation.en.see_photos_6;
+    see_photos_7.innerText = translation.en.see_photos_7;
+    see_photos_8.innerText = translation.en.see_photos_8;
+    see_photos_9.innerText = translation.en.see_photos_9;
+    see_photos_10.innerText = translation.en.see_photos_10;
+    see_photos_11.innerText = translation.en.see_photos_11;
+    see_photos_12.innerText = translation.en.see_photos_12;
+    see_photos_13.innerText = translation.en.see_photos_13;
+    see_photos_14.innerText = translation.en.see_photos_14;
+    see_photos_15.innerText = translation.en.see_photos_15;
+    see_photos_16.innerText = translation.en.see_photos_16;
+    see_photos_17.innerText = translation.en.see_photos_17;
+    see_photos_18.innerText = translation.en.see_photos_18;
+    see_photos_19.innerText = translation.en.see_photos_19;
+    see_photos_20.innerText = translation.en.see_photos_20;
+    p_4.innerText = translation.en.p_4;
+    p_5.innerText = translation.en.p_5;
+    p_6.innerText = translation.en.p_6;
+    p_7.innerText = translation.en.p_7;
+    p_8.innerText = translation.en.p_8;
+    p_9.innerText = translation.en.p_9;
+    p_10.innerText = translation.en.p_10;
+    p_11.innerText = translation.en.p_11;
+    p_12.innerText = translation.en.p_12;
+    p_13.innerText = translation.en.p_13;
+    p_14.innerText = translation.en.p_14;
+    p_15.innerText = translation.en.p_15;
+    p_16.innerText = translation.en.p_16;
+    p_17.innerText = translation.en.p_17;
+    p_18.innerText = translation.en.p_18;
+    p_19.innerText = translation.en.p_19;
+    p_20.innerText = translation.en.p_20;
+
+  }
+}
+
+
+
+function myFunction(x) {
+    x.classList.toggle("change");
+  }
